569f31316f024e150d0c9402eabcf248c0d804ce
pengdong <pengdong@fengbangleasing.com>
2018-08-17 18:32:31 +0800

    添加 css 模块化配置

--- www/src/pages/index.jsx
+++ www/src/pages/index.jsx
@@ -3,15 +3,16 @@ import { Link, Route } from "react-router-dom";
 import logo from "../assets/logo.svg"
 import Blog from "./blog"
 import User from "./user"
-import "../style/index.css"
+import Login from "./login"
+import style from "../style/index.css"
 
 class App extends Component {
   render() {
     return (
-      <div className="index">
-        <header className="nav">
-          <div className="f-fl logo-box">
-            <img src={logo} className="logo" alt="logo" />
+      <div id={style.index}>
+        <header className={style.nav}>
+          <div className={style.logo_box + ' f-fl'}>
+            <img src={logo} className={style.logo} alt="logo" />
           </div>
           <ul>
             <li><Link to="/">首页</Link></li>
@@ -21,16 +22,14 @@ class App extends Component {
             <li><Link to="/blog">商店</Link></li>
             <li><Link to="/blog">论坛</Link></li>
           </ul>
-          <div className="login">
-          <a>登录</a>
+          <div className={style.login}>
+            <Link to="/login">登录</Link>
           </div>
         </header>
-        <div className="body">
-          <div className="content">
-            <Route path="/user" component={User} />
-            <Route path="/blog" component={Blog} />
-          </div>
-          <div className="right-box">{this.props.children}</div>
+        <div>
+          <Route path="/user" component={User} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/login" component={Login} />
         </div>
       </div>
     );
--- www/src/pages/login.jsx
+++ www/src/pages/login.jsx
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
-
+import style from "../style/login.css"
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { tabName: "login" };
+  }
+
+  handleClick(tabName, e) {
+
+    this.setState({ tabName: tabName });
+  }
+
   render() {
     return (
-      <div className="user">
-
+      <div id={style.login}>
+        <div className={style.ct_box}>
+          <div className={style.nav}>
+            <ul>
+              <li onClick={this.handleClick.bind(this, "login")}>登录</li>
+              <li onClick={this.handleClick.bind(this, "register")}>注册</li>
+            </ul>
+          </div>
+          <TabContent tabName={this.state.tabName} />
+        </div>
       </div>
     );
   }
 }
 
+function TabContent(props) {
+  return (
+    <div className={style.form}>
+      {props.tabName === "register" ?
+        (<div className={style.form_item}>
+          <label htmlFor="username">邮箱</label>
+          <input type="email" name="username" id="username" />
+        </div>) : ""}
+      <div className={style.form_item}>
+        <label htmlFor="username">用户名</label>
+        <input type="text" name="username" id="username" />
+      </div>
+      <div className={style.form_item}>
+        <label htmlFor="password">密码</label>
+        <input type="text" name="password" id="password" />
+      </div>
+      <div className={style.submit}>
+        <input type="submit" value="上传" />
+      </div>
+    </div>
+  )
+}
 export default App;
